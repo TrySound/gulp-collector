02683222d7c053d5e9a2fcf157a49d61478b10c4
Bogdan Chadkin <trysound@yandex.ru>
2015-03-07 02:41:28 +0300

    initial

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # gulp-collector
 Collect files from pipeline
 
+## Usage
+
 ```
 npm i gulp-collector
 ```
@@ -10,16 +12,21 @@ var gulp = require('gulp'),
 	collector = require('gulp-collector');
 
 gulp.task('default', function () {
-	gulp.src('components/**/*.*')
+	gulp.src('components/**/index.{html,css}')
 		.pipe(collector(function (src) {
-			return {
-				filename: 'compiled.html',
-				content: src['index.html'] + '\n===\n' + src['index.css']
-			};
-		}, ['index.html', 'index.css']))
+			// Src - object with {filename: content} of files in the same directory
+			// return { filename: content }
+			return { 'compiled.html': src['index.html'] + '\n===\n' + src['index.css'] };
+		}, options))
 });
 
 ```
+
+### options
+
+- `options.cache` ('default') - cache namespace
+- `options.base` ('.') - glob base
+
 
 
 ##License
---
+++ index.js
@@ -0,0 +1,78 @@
+var through = require('through2'),
+	gutil = require('gulp-util'),
+	extend = require('extend'),
+	path_lib = require('path'),
+	cache = {};
+
+
+function collector(fn, opts) {
+	opts = opts || {};
+	opts.cache = typeof opts.cache === 'string' ? opts.cache : 'default';
+	opts.base = typeof opts.base === 'string' ? opts.base : '.';
+
+
+	var registry = opts.cache ? (cache[opts.cache] = cache[opts.cache] || {}) : {},
+		active = [];
+
+	return through.obj(function (file, enc, cb) {
+		var dir,
+			result,
+			push = this.push.bind(this);
+
+		if(file.isNull()) {
+			cb();
+			return;
+		}
+
+		if(file.isStream()) {
+			cb(new gutil.PluginError('gulp-collector',  'Streaming not supported'));
+			return;
+		}
+
+		if(typeof fn === 'function') {
+			dir = path_lib.dirname(file.path)
+
+			if( ! (dir in registry)) {
+				registry[dir] = {};
+			}
+
+			if(active.indexOf(dir) === -1) {
+				active.push(dir);
+			}
+			registry[dir][path_lib.basename(file.path)] = file.contents.toString();
+		}
+
+		cb();
+
+	}, function (cb) {
+		var push = this.push.bind(this);
+
+		active.forEach(function (dir) {
+			var files = registry[dir],
+				result;
+
+			if(Object.keys(files).length) {
+				result = fn(extend({}, files));
+
+				if(typeof result === 'object') {
+					Object.keys(result).forEach(function (filename) {
+						var file;
+
+						if(filename) {
+							push(new gutil.File({
+								base: opts.base,
+								path: [dir, filename].join('/'),
+								contents: new Buffer(result[filename])
+							}));
+						}
+					});
+				}
+			}
+		});
+
+		cb();
+	});
+};
+
+
+module.exports = collector;
