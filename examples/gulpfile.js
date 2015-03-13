a2b63d4bb259511e67ae9a458294a3f837cfe403
Богдан Чадкин <trysound@yandex.ru>
2015-03-14 00:11:01 +0300

    Added example

---
+++ examples/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+	collector = require('../index');
+
+gulp.task('default', function () {
+	var dst = '';
+	return gulp.src('src/*.*')
+		.pipe(collector(function (files) {
+			Object.keys(files).forEach(function (filename) {
+				dst += files[filename];
+			});
+		}, {
+			end: function () {
+				return {
+					'result.txt': dst
+				};
+			}
+		}))
+		.pipe(gulp.dest('dst'));
+});
